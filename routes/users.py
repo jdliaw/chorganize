dc96503c6e475ae65b121e2035fa44a0c7b5eb6c
CLICC User <youaregreathk@gmail.com>
2017-11-07 19:01:03 -0800

    Add user controler for edit, delete features

--- routes/users.py
+++ routes/users.py
@@ -1,37 +1,91 @@
 from . import routes
 from flask import request
-from database_setup import User
+from database_setup import User, db
 from flask.json import loads, jsonify
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
-@routes.route('/api/user', methods=['GET', 'POST'])
-def user():
-    if request.method == 'POST':
-        data = request.data
-        dataDict = loads(data)
+@routes.route('/api/user', methods=['POST'])
+def createUser():
+    data = request.data
+    dataDict = loads(data)
+    try:
         userEmail = dataDict['email']
         userName = dataDict['username']
         userPassword = dataDict['password']
+        userFirstName = dataDict['firstname']
+        userLastName = dataDict['lastname']
+    except KeyError:
+        error = "Invalid input Parameters"
+        return error, 400
 
-        try:
-            User.createUser(userEmail, userName, userPassword)
-        except IntegrityError as e:
-            err = str(e)
-            if ("UNIQUE constraint failed: user.email" in err):
-                return "User Exists"
-            elif ("NOT NULL constraint failed: user.username" in err):
-                return "Username None"
-            elif ("NOT NULL constraint failed: user.password" in err):
-                return "Password None"
+    try:
+        User.createUser(userEmail, userName, userPassword, userFirstName, userLastName)
+    except IntegrityError:
+        error = "Failed to create new user"
+        return error, 500
 
-        return "User Successfully Created"
+    return "User Successfully Created"
 
-    elif request.method == 'GET':
-        userEmail = request.args.get('email')
-        try:
-            user = User.getUser(userEmail)
-        except NoResultFound:
-            return "User Not Found"
 
-        return jsonify(user.serialize)
+@routes.route('/api/user', methods=['GET'])
+def getUser():
+    userEmail = request.args.get('email')
+    if userEmail is None:
+        error = "Invalid input Parameters"
+        return error, 400
+    try:
+        user = User.getUser(userEmail)
+    except NoResultFound:
+        error = "User Not found"
+        return error, 404
+
+    return jsonify(user.serialize)
+
+# Edit
+@routes.route('/api/user', methods=['PUT'])
+def modifyUser():
+    data = request.data
+    dataDict = loads(data)
+    try:
+        usernewEmail = dataDict['newemail']
+        useroldEmail = dataDict['oldemail']
+        userName = dataDict['username']
+        userPassword = dataDict['password']
+        userFirstName = dataDict['firstname']
+        userLastName = dataDict['lastname']
+    except KeyError:
+        error = "Invalid input Parameters"
+        return error, 400
+
+    user = User.query.filter_by(email=useroldEmail).one()
+    user.setEmail(usernewEmail)
+    user.setPassword(userPassword)
+    user.setFirstname(userFirstName)
+    user.setLastname(userLastName)
+    user.setUsername(userName)
+    db.session.commit()
+
+    return "Successfully Modified"
+
+
+
+#Delete User
+@routes.route('/api/user/delete', methods=['POST'])
+def deleteUser():
+    data = request.data
+    dataDict = loads(data)
+
+    try:
+        userEmail = dataDict['email']
+    except KeyError:
+        error = "Invalid input Parameters"
+        return error, 400
+
+    try:
+        User.deleteUser(userEmail)
+    except NoResultFound:
+        error = "User Not found"
+        return error, 404
+
+    return "User Successfully Romved"
