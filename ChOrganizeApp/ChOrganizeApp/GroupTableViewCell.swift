c57cb3f2f4d764a976e53be0e98b3eb73dc42cda
Isaac Kim <isaackim1122@yahoo.com>
2017-11-11 15:52:40 -0800

    Group Stuff

---
+++ ChOrganizeApp/ChOrganizeApp/GroupTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  GroupTableViewCell.swift
+//  ChOrganizeApp
+//
+//  Created by CLICC User on 11/11/17.
+//  Copyright © 2017 Pusheen Code. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+//    @IBOutlet weak var cell: GroupTableViewCell!
+    
+    @IBOutlet weak var cell: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+  
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
