3502c4664ae73e8305a5d203c1e097fc56874474
Michael Shea <youaregreathk@gmail.com>
2017-11-12 17:06:40 -0800

    Add test cases for User

--- Tests/test_user.py
+++ Tests/test_user.py
@@ -8,9 +8,13 @@ class TestUser(unittest.TestCase):
     def test_2(self):
         self.assertTrue(True)
 
-    def test_get_user_failsWithInvalidInput(self):
+    def test_get_user_failsWithMissingInput(self):
         response2 = requests.get("http://localhost:8080/api/user")
-        self.assertEquals(response2.status_code, 400)
+        self.assertEqual(response2.status_code, 400)
+
+    def test_get_user_failsWithInvalidInput(self):
+        response2 = requests.get("http://localhost:8080/api/user?name=hahaha")
+        self.assertEqual(response2.status_code, 400)
 
 if __name__ == '__main__':
     unittest.main()
