c54b601df8ffd22c91e6e5acee589a0d9c64d42c
Kaitlyne Chan <kchan52@ucla.edu>
2017-11-15 18:58:58 -0800

    Query database in unit tests to check that stuff is added/deleted properly

--- Tests/test_chores.py
+++ Tests/test_chores.py
@@ -4,6 +4,7 @@ sys.path.append('..')
 from database_setup import createApp, db, User, Group, Chore
 import requests
 import json
+import sqlalchemy.orm.exc
 
 class TestChores(unittest.TestCase):
     def setUp(self):
@@ -16,10 +17,21 @@ class TestChores(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        
+    def addChoreToDB(self, name):
+        chore = Chore.createChore(name)
+        self.group.addChore(chore)
     
     def test_create_chore(self):
         response = requests.post('http://localhost:8080/api/chore/create', data='{"name": "vacuum", "groupID": 1}')
         self.assertEqual(response.status_code, 200)
+        
+        chore = Chore.getChore(1)
+        choreName = chore.getName()
+        groupID = chore.getGroupID()
+        
+        self.assertEqual(choreName, "vacuum")
+        self.assertEqual(groupID, 1)
        
     def test_create_chore_missing_input(self):
         response = requests.post('http://localhost:8080/api/chore/create', data='{"name": "clean bathroom"}')
@@ -31,6 +43,19 @@ class TestChores(unittest.TestCase):
                                 )
         self.assertEqual(response.status_code, 200)
         
+        chore = Chore.getChore(1)
+        choreName = chore.getName()
+        groupID = chore.getGroupID()
+        deadline = chore.getDeadline()
+        description = chore.getDescription()
+        userEmail = chore.getUserEmail()
+        
+        self.assertEqual(choreName, "sweep")
+        self.assertEqual(groupID, 1)
+        self.assertEqual(deadline.strftime('%m/%d/%Y'), "11/20/2017")
+        self.assertEqual(description, "sweep bedroom and living room")
+        self.assertEqual(userEmail, "kchan52@ucla.edu")
+        
     def test_create_chore_invalid_input(self):
         response = requests.post('http://localhost:8080/api/chore/create',
                                  data='{"name": "take out trash", "groupID": 1, "userEmail": "idontexist@gmail.com"}'
@@ -38,8 +63,7 @@ class TestChores(unittest.TestCase):
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_get_chore(self):
-        chore = Chore.createChore("wash dishes")
-        self.group.addChore(chore)
+        self.addChoreToDB("wash dishes")
         response = requests.get('http://localhost:8080/api/chore/get?id=1')
         self.assertEqual(response.status_code, 200)
         
@@ -52,50 +76,48 @@ class TestChores(unittest.TestCase):
         self.assertEqual(attributesPresent, True)
     
     def test_get_chore_missing_input(self):
-        chore = Chore.createChore("clean sink")
-        self.group.addChore(chore)
+        self.addChoreToDB("clean sink")
         response = requests.get('http://localhost:8080/api/chore/get')
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_get_chore_invalid_input(self):
-        chore = Chore.createChore("clean shower")
-        self.group.addChore(chore)
+        self.addChoreToDB("clean shower")
         response = requests.get('http://localhost:8080/api/chore/get?id=5')
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_modify_chore_name(self):
-        chore = Chore.createChore("vacuum")
-        self.group.addChore(chore)
+        self.addChoreToDB("vacuum")
         response = requests.put('http://localhost:8080/api/chore/modify', data='{"id": 1, "name": "vacuum bedroom"}')
         self.assertEqual(response.status_code, 200)
         
+        chore = Chore.getChore(1)
+        choreName = chore.getName()
+        
+        self.assertEqual(choreName, "vacuum bedroom")
+        
     def test_modify_chore_missing_input(self):
-        chore = Chore.createChore("vacuum")
-        self.group.addChore(chore)
+        self.addChoreToDB("vacuum")
         response = requests.put('http://localhost:8080/api/chore/modify', data='{"name": "vacuum bedroom"}')
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_modify_chore_invalid_input(self):
-        chore = Chore.createChore("vacuum")
-        self.group.addChore(chore)
+        self.addChoreToDB("vacuum")
         response = requests.put('http://localhost:8080/api/chore/modify', data='{"id": 4, "deadline": "12/31/2017"}')
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_delete_chore(self):
-        chore = Chore.createChore("dust shelves")
-        self.group.addChore(chore)
+        self.addChoreToDB("dust shelves")
         response = requests.delete('http://localhost:8080/api/chore/delete', data='{"id": 1}')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)       
+        self.assertRaises(sqlalchemy.orm.exc.NoResultFound, Chore.getChore, 1)
         
     def test_delete_chore_missing_input(self):
-        chore = Chore.createChore("dust shelves")
-        self.group.addChore(chore)
+        self.addChoreToDB("dust shelves")
         response = requests.delete('http://localhost:8080/api/chore/delete', data='{}')
         self.assertGreaterEqual(response.status_code, 400)
         
     def test_delete_chore_invalid_input(self):
-        chore = Chore.createChore("dust shelves")
-        self.group.addChore(chore)
+        self.addChoreToDB("dust shelves")
         response = requests.delete('http://localhost:8080/api/chore/delete', data='{"id": 0}')
         self.assertGreaterEqual(response.status_code, 400)
         
