133f31bd893be88c811abe02c052e2b5957ebcb8
jdliaw <jdliaw@ucla.edu>
2017-12-04 20:48:53 -0800

    Add signup page, clicking logout takes you to front page

---
+++ ChOrganizeApp/ChOrganizeApp/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  ChOrganizeApp
+//
+//  Created by CLICC User on 12/4/17.
+//  Copyright © 2017 Pusheen Code. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func logoutAction(_ sender: Any) {
+        
+        // Transition back to login page
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "LoginVC") as UIViewController
+        self.present(nextViewController, animated:true, completion:nil)
+    }
+}
---
+++ ChOrganizeApp/ChOrganizeApp/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  ChOrganizeApp
+//
+//  Created by CLICC User on 12/4/17.
+//  Copyright © 2017 Pusheen Code. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signupAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
