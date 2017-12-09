7af844a95bab6a014ded83cb17177a721dc140f6
Michael Shea <shea@Michaels-MacBook-Pro.local>
2017-12-08 20:08:18 -0800

    Use bcrypt to has password

--- routes/users.py
+++ routes/users.py
@@ -5,40 +5,48 @@ from flask.json import loads, jsonify
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
+import bcrypt
+
+
 @routes.route('/api/user/create', methods=['POST'])
 def createUser():
     """
-    Create a new user and add to the database.
-    
+    Create a new user and add to the database, use Bcrypt to hash password
+
     :param email: the email of the user
-    :param username: the username of the user
+    :param username: (optional) the username of the user
     :param password: the password of the user
-    :param firstName: the first name of the user 
-    :param lastName: the last name of the user
-    
+    :param firstName: the first name of the user
+    :param lastName: (optional) the last name of the user
+
     :type email: str
     :type username: str
     :type password: str
     :type firstName: str
     :type lastName: str
-    
+
     :return: "User Successfully Created", status code
     :rtype: str, int
-    
+
     :raises KeyError: if the input is not provided by the user
-    :raises sqlalchemy.exc.IntegrityError: if the user already existed in the database 
+    :raises sqlalchemy.exc.IntegrityError: if the user already existed in the database
     """
     data = request.data
     dataDict = loads(data)
+
     try:
         userEmail = dataDict['email']
-        userName = dataDict['username']
         userPassword = dataDict['password']
         userFirstName = dataDict['firstName']
-        userLastName = dataDict['lastName']
     except KeyError:
         error = "Invalid input Parameters"
         return error, 400
+
+    salt = bcrypt.gensalt()
+    userPassword = bcrypt.hashpw(userPassword.encode(), salt)
+
+    userName = dataDict.get('username', userEmail)
+    userLastName = dataDict.get('lastName', None)
 
     try:
         User.createUser(userEmail, userName, userPassword, userFirstName, userLastName)
@@ -49,17 +57,16 @@ def createUser():
     return "User Successfully Created"
 
 
-    
 @routes.route('/api/user/get', methods=['GET'])
 def getUser():
     """
     Get information about a user.
-    
+
     :param email: the email of the user
     :type email: str
     :return: a user JSON object, status code
     :rtype: JSON object, int
-    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database 
+    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database
     """
     userEmail = request.args.get('email')
     if userEmail is None:
@@ -74,68 +81,70 @@ def getUser():
     return jsonify(user.serialize)
 
 
-
-#Edit User
+# Edit User
 @routes.route('/api/user/edit', methods=['PUT'])
 def modifyUser():
     """
     Modify fields of a User object.
-    
-    :param newemail: the new email of the user
+
+    :param newemail: (optional) the new email of the user
     :param oldemail: the original email of the user
-    :param username: the new username of the user
-    :param password: the new password of the user
-    :param firstName: the new firstName of the user
-    :param lastName: the new lastName of the user
-    
+    :param username: (optional) the new username of the user
+    :param password: (optional) the new password of the user
+    :param firstName:(optional)the new firstName of the user
+    :param lastName: (optional) the new lastName of the user
+
     :type newemail: str
     :type oldemail: str
     :type username: str
     :type password: str
     :type firstName: str
     :type lastName: str
-    
+
     :return: "Successfully Modified", status code
     :rtype: str, int
-    
+
     :raises KeyError: if the input is not provided by the user
     """
     data = request.data
     dataDict = loads(data)
     try:
-        usernewEmail = dataDict['newemail']
         useroldEmail = dataDict['oldemail']
-        userName = dataDict['username']
-        userPassword = dataDict['password']
-        userFirstName = dataDict['firstName']
-        userLastName = dataDict['lastName']
     except KeyError:
         error = "Invalid input Parameters"
         return error, 400
 
     user = User.query.filter_by(email=useroldEmail).one()
-    user.setEmail(usernewEmail)
-    user.setPassword(userPassword)
-    user.setFirstname(userFirstName)
-    user.setLastname(userLastName)
-    user.setUsername(userName)
-
+    if 'newemail' in dataDict:
+        user.setEmail(dataDict['newemail'])
+    if 'password' in dataDict:
+        '''
+        hash_object = hashlib.md5(dataDict['password'].encode())
+        userPassword = hash_object.hexdigest()
+        user.setPassword(userPassword)
+        '''
+        user.setPassword(dataDict['password'])
+    if 'firstName' in dataDict:
+        user.setFirstName(dataDict['firstName'])
+    if 'lastName' in dataDict:
+        user.setLastName(dataDict['lastName'])
+    if 'username' in dataDict:
+        user.setUserName(dataDict['username'])
     return "Successfully Modified"
 
 
-
-#Delete User
+# Delete User
 @routes.route('/api/user/delete', methods=['POST'])
 def deleteUser():
     """
     Delete a user from the database.
-    
+
     :param email: the email of the user
     :type email: str
     :return: "User Successfully Removed", status code
     :rtype: str, int
     :raises KeyError: if the input is not provided by the user
-    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database 
+    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database
     """
     data = request.data
     dataDict = loads(data)
@@ -155,26 +164,25 @@ def deleteUser():
     return "User Successfully Removed"
 
 
-
-#Get all active chores or completed chores for a particular user in a particular group
+# Get all active chores or completed chores for a particular user in a particular group
 @routes.route('/api/user/get-unfinished-chores', methods=['GET'])
 def getChores():
     """
     Get all active chores or completed chores for a particular user in a particular group.
-    
+
     :param email: the email of the user
     :param groupID: a particular groupID that the user is in
-    :param completed: a boolean indicating whether a list of active chores or completed chores get return 
-    
+    :param completed: a boolean indicating whether a list of active chores or completed chores get return
+
     :type email: str
     :type groupID: int
     :type completed: boolean
-    
+
     :return: a list of JSON chore objects, status code
     :rtype: list of JSON objects, int
-    
+
     :raises ValueError: if the input of the completed parameter is neither true or false
-    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database 
+    :raises sqlalchemy.orm.exc.NoResultFound: if the user is not found in the database
     """
     userEmail = request.args.get('email')
     groupID = request.args.get('groupID')
