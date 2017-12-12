08fbcd66b6bda35ed959eb63247ec8e532598558
jdliaw <jdliaw@ucla.edu>
2017-12-11 16:07:40 -0800

    Persist user data in NSUserDefaults after login

--- ChOrganizeApp/ChOrganizeApp/LoginViewController.swift
+++ ChOrganizeApp/ChOrganizeApp/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginViewController: UIViewController {
     let BASE_URL = "http://shea3100.pythonanywhere.com"
     let VALIDATE_USER_URL = "/api/user/validate-password"
+    let GET_USER_URL = "/api/user/get"
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -21,24 +22,6 @@ class LoginViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    private func moveToToDo() {
-        print("in move to")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let TabBarVC = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.window?.rootViewController = TabBarVC
-    }
-    
-    private func loginErrorAlert(message: String) {
-        let alert = UIAlertController(
-            title: "Error",
-            message: message,
-            preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: nil))
-        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func userLogin(_ sender: Any) {
@@ -67,6 +50,7 @@ class LoginViewController: UIViewController {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         
+        // serialize body for POST
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
         } catch let error {
@@ -76,12 +60,12 @@ class LoginViewController: UIViewController {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("appilcation/json", forHTTPHeaderField: "Accept")
 
+        // perform request
         let task = URLSession.shared.dataTask(with: request){ data, response, error in
             guard let data = data, error == nil else {
                 print("error=\(error)")
                 return
             }
-            
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(response)")
@@ -99,7 +83,7 @@ class LoginViewController: UIViewController {
                 // if result true, move to app homepage
                 if result! {
                     DispatchQueue.main.async {
-                        self.moveToToDo()
+                        self.loginSuccessHandler(email: emailInput!)
                     }
                 }
                 else {
@@ -113,5 +97,102 @@ class LoginViewController: UIViewController {
             }
         }
         task.resume()
+    }
+    
+    private func getUserRequest(email: String){// -> Dictionary<String, String> {
+        // setup GET request for GET_USER_URL
+        var components = URLComponents(string: BASE_URL + GET_USER_URL)!
+        components.queryItems = [URLQueryItem(name: "email", value: email)]
+        var request = URLRequest(url: components.url!)
+        
+        // perform request
+        let task = URLSession.shared.dataTask(with: request){ data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(error)")
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            // request success
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+            // parse response. only successful login if the result is true
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: Any]
+                let firstName = json["firstName"] as? String ?? nil
+                let lastName = json["lastName"] as? String ?? nil
+                let username = json["username"] as? String ?? nil
+                print("returned object")
+                print(firstName)
+                
+                let userInfo = [
+                    "firstName": firstName,
+                    "lastName": lastName,
+                    "username": username
+                ]
+                //return userInfo // return user object
+//                
+//                DispatchQueue.main.async {
+//                    let userInfo = [
+//                        "firstName": firstName,
+//                        "lastName": lastName,
+//                        "username": username
+//                    ]
+//                    return userInfo // return user object
+//                }
+                
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    private func loginSuccessHandler(email: String) {
+        print("in handler")
+        // TODO: use NSUSerDefaults to persist user email across app
+        let defaults = UserDefaults.standard
+        defaults.setValue(email, forKey: "email")
+        
+        let result = UserDefaults.standard.value(forKey: "email")
+        print(result!)
+        
+        // make request to get user's name (to show in settings)
+       // var userInfo = getUserRequest(email: email)
+//        defaults.setValue(userInfo["firstName"], forKey: "firstName")
+//        if userInfo["lastName"] != nil {
+//            defaults.setValue(userInfo["lastName"], forKey: "lastName")
+//        }
+//        if userInfo["username"] != nil {
+//            defaults.setValue(userInfo["username"], forKey: "username")
+//        }
+//        
+//        print("after get")
+//        let name = UserDefaults.standard.value(forKey: "firstName")
+//        print(name!)
+//        
+        moveToToDo()
+    }
+    
+    private func moveToToDo() {
+        print("in move to")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let TabBarVC = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window?.rootViewController = TabBarVC
+    }
+    
+    private func loginErrorAlert(message: String) {
+        let alert = UIAlertController(
+            title: "Error",
+            message: message,
+            preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
