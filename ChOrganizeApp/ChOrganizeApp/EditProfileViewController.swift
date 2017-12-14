f01323c67954bfc821d348105203ef1a62f2ce76
Hana Kim <hanakim96@gmail.com>
2017-12-13 16:15:58 -0800

    New files

---
+++ ChOrganizeApp/ChOrganizeApp/EditProfileViewController.swift
@@ -0,0 +1,46 @@
+//
+//  EditProfileViewController.swift
+//  ChOrganizeApp
+//
+//  Created by Hana on 12/11/17.
+//  Copyright © 2017 Pusheen Code. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+        dismiss()
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss()
+    }
+    func dismiss() {
+        self.navigationController?.popViewController(animated: true)
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
