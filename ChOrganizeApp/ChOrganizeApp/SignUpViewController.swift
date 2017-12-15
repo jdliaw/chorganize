58985c49fff4b11085f4610de77731156f909d94
CLICC User <isaackim1122@yahoo.com>
2017-12-14 19:32:04 -0800

    Sign up page text fields now don't get blocked by keyboard

--- ChOrganizeApp/ChOrganizeApp/SignUpViewController.swift
+++ ChOrganizeApp/ChOrganizeApp/SignUpViewController.swift
@@ -8,22 +8,54 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+        self.firstNameField.delegate = self
+        self.lastNameField.delegate = self
         // Do any additional setup after loading the view.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+  
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField == emailField || textField == passwordField){
+        ScrollView.setContentOffset(CGPoint(x:0,y:100), animated: true)
+        }
+        if(textField == lastNameField){
+        ScrollView.setContentOffset(CGPoint(x:0,y:50), animated: true)
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (textField == emailField || textField == passwordField || textField == lastNameField){
+        
+        ScrollView.setContentOffset(CGPoint(x:0,y:0), animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     @IBAction func signupAction(_ sender: Any) {
