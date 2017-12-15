9a0ca0394ce28a89a634bc52c73ef13e8db51a2d
Michael Shea <youaregreathk@gmail.com>
2017-12-14 19:56:25 -0800

    Add test cases for user

--- Tests/test_user.py
+++ Tests/test_user.py
@@ -27,7 +27,7 @@ class TestUser(unittest.TestCase):
         db.drop_all()
 
     def test_delete_user(self):
-        response2 = requests.put('http://localhost:8080/api/user/delete', data='{"email": "hello@ucla.edu}')
+        response2 = requests.post('http://localhost:8080/api/user/delete', data='{"email": "hello@ucla.edu}')
         self.assertEqual(response2.status_code, "User Successfully Removed")
 
     def test_get_user_fails_with_user_not_found(self):
@@ -36,25 +36,38 @@ class TestUser(unittest.TestCase):
 
 
     def test_get_user_fails_with_missing_input(self):
-        response2 = requests.get("http://localhost:8080/api/user")
+        response2 = requests.get("http://localhost:8080/api/user/get")
         self.assertEqual(response2.status_code, 400)
 
     def test_get_user_fails_with_invalid_input(self):
-        response2 = requests.get("http://localhost:8080/api/user?name=hahaha")
+        response2 = requests.get("http://localhost:8080/api/user/get?name=hahaha")
         self.assertEqual(response2.status_code, 400)
 
     def test_modify_user_fail_with_missing_input(self):
-        response2 = requests.put('http://localhost:8080/api/user', data='{"userName": "vacuum", "userPassword": "134"}')
+        response2 = requests.put('http://localhost:8080/api/user/edit', data='{"userName": "vacuum", "userPassword": "134"}')
         self.assertEqual(response2.status_code, 400)
 
     def test_delete_user_fail_with_invalid_input(self):
-        response2 = requests.put('http://localhost:8080/api/user/delete', data='{"userName": "vacuum"}')
+        response2 = requests.post('http://localhost:8080/api/user/delete', data='{"userName": "vacuum"}')
         self.assertEqual(response2.status_code, 400)
 
     def test_get_chores_for_a_particular_user_in_a_particular_group_fail_with_invalid_input(self):
-        response2 = requests.get('http://localhost:8080/api/user/getUnfinisihedChores', data='{"userName": "hahaha"}')
+        response2 = requests.get('http://localhost:8080/api/user/get-unfinisihed-chores', data='{"userName": "hahaha"}')
         self.assertEqual(response2.status_code, 400)
 
+    def test_validate_password_fail_with_missing_input(self):
+        response2 = requests.post('http://localhost:8080/api/user', data='{"password": "123"}')
+        self.assertEqual(response2.status_code, 404)
+
+    def test_validate_password_with_correct_passowrd_and_user_email(self):
+        response2 = requests.post('http://localhost:8080/api/user/validate-password',data='{"email": "michael@ucla.edu","password": "123"}')
+        data = response2.json()
+        self.assertEqual(data["result"], "true")
+
+    def test_validate_password_with_incorrect_passowrd(self):
+        response2 = requests.post('http://localhost:8080/api/user/validate-password',data='{"email": "michael@ucla.edu","password": "11123"}')
+        data = response2.json()
+        self.assertEqual(data["result"], "false")
 
 
 if __name__ == '__main__':
